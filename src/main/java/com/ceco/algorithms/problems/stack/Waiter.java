aba5cad212cb35ad9b8422102a993c86844dbf77
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2016-09-14 10:11:09 +0300

    Fixed sieve of eratosthenes in Waiter task

--- src/main/java/com/ceco/algorithms/problems/stack/Waiter.java
+++ src/main/java/com/ceco/algorithms/problems/stack/Waiter.java
@@ -1,11 +1,23 @@
 package com.ceco.algorithms.problems.stack;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.*;
 
 /**
+ * @see <a href="https://www.hackerrank.com/challenges/waiter">
+ *          Waiter
+ *     </a>
+ *
+ * Example input:
+ * 5 1
+ * 3 4 7 6 5
+ *
+ * Output:
+ * 4
+ * 6
+ * 3
+ * 7
+ * 5
+ *
  * @author Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
  * @since 13-Sep-2016
  */
@@ -22,7 +34,7 @@ public class Waiter {
             arr[i] = scanner.nextInt();
         }
 
-        int[] primes = sieveOfEratosthenes(platePiles);
+        List<Integer> primes = sieveOfEratosthenes(platePiles + 1);
 
         Deque<Integer> nonDivisable = new ArrayDeque<>();
 
@@ -59,31 +71,21 @@ public class Waiter {
         return otherNonDivisable;
     }
 
-    private static int[] sieveOfEratosthenes(int primesCount) {
-        int[] primes = new int[primesCount];
+    private static List<Integer> sieveOfEratosthenes(int primesCount) {
+        List<Integer> primes = new ArrayList<>();
         boolean[] primesStatus = new boolean[primesCount + 1];
         Arrays.fill(primesStatus, true);
-        if (primesCount == 1) {
-            primes[0] = 2;
-            return primes;
-        } else if (primesCount == 2) {
-            primes[0] = 2;
-            primes[1] = 3;
-            return primes;
-        }
-        
+
         for (int i = 2; i < Math.sqrt(primesCount); i++) {
             if (primesStatus[i]) {
-                for (int j = i * i; j <= primesCount; j += i) {
+                for (int j = i * i; j <= primesCount; j += i)
                     primesStatus[j] = false;
-                }
             }
         }
 
         for (int i = 2; i <= primesCount; i++) {
-            if (primesStatus[i]) {
-                primes[i] = i;
-                System.out.println(primes[i]);
+            if (primesStatus[i - 1]) {
+                primes.add(i);
             }
         }
 
