769108f2168284a93ef32a3eb03ea6d4bd8baf11
Tsvetan Dimitrov <tsvetan.dimitrov23@gmail.com>
2016-09-23 15:16:39 +0200

    Removed unneeded second stack in MaximumElement

--- src/main/java/com/ceco/algorithms/problems/stack/MaximumElement.java
+++ src/main/java/com/ceco/algorithms/problems/stack/MaximumElement.java
@@ -34,7 +34,6 @@ public class MaximumElement {
         int queriesCount = scanner.nextInt();
         scanner.nextLine();
 
-        Deque<Integer> stack = new ArrayDeque<>();
         Deque<Integer> maxStack = new ArrayDeque<>();
         List<Integer> maxElems = new ArrayList<>();
 
@@ -48,14 +47,12 @@ public class MaximumElement {
 
             switch (queryType) {
                 case 1:
-                    // push to main stack
-                    stack.push(queryInput);
                     if (!maxStack.isEmpty()) {
-                        // maintain an alternative stack that is descendingly sorted
+                        // maintain a stack that is descendingly sorted
                         // if the top is less than the current input push it as the new top
                         // else replicate the current top as a new pushed entry to maintain
-                        // same length on both stacks in order for the max stack to keep the
-                        // current maximum even after a pop query on both stacks
+                        // same length on both stacks in order for the stack to keep the
+                        // current maximum even after a pop query on it
                         if (maxStack.peek() < queryInput) {
                             maxStack.push(queryInput);
                         } else {
@@ -66,7 +63,6 @@ public class MaximumElement {
                     }
                     break;
                 case 2:
-                    stack.pop();
                     maxStack.pop();
                     break;
                 case 3:
